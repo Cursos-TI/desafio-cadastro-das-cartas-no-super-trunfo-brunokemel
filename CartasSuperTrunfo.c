21b826acb7f41c70dab8d76790bb8d8d48c8d85e
Bruno Kemel <br.kemel@gmail.com>
2025-08-27 17:06:48 -0300

    primeira parte do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,10 +7,63 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    //Carta, Estado, Código, Nome Cidade, população, área, PIB, Num. DP turis
+    // Usei o Struck para criar e organazar melhor as cartas separadamente  
+    struct CartaSuperTrunfo  {
+        char carta[3];
+        char estado[20];
+        int codigo;
+        char cidade[20];
+        int populacao;
+        float area;
+        float pib;
+        int NPT;
+    };
+
+    struct CartaSuperTrunfo carta1 = {
+        "01",
+        "Para",
+        1,
+        "Belem",
+        1500000,
+        1068.0,
+        50000.0,
+        5
+    };
+
+    struct CartaSuperTrunfo carta2 = {
+        "02",
+        "Bahia",
+        2,
+        "Salvador",
+        1600000,
+        1070.0,
+        80000.0,
+        7
+    };
+
+    printf("Carta: %s\n", carta1.carta);
+    printf("Estado: %s\n", carta1.estado);
+    printf("Codigo: %d\n", carta1.codigo);
+    printf("Cidade: %s\n", carta1.cidade);
+    printf("Populacao: %d\n", carta1.populacao);
+    printf("Area: %.2f km quadrados\n", carta1.area);
+    printf("PIB: %.2f bilhoes\n", carta1.pib);
+    printf("N de pontos turisticos: %d\n", carta1.NPT);
+
+    printf("\n");
+
+    printf("Carta: %s\n", carta2.carta);
+    printf("Estado: %s\n", carta2.estado);
+    printf("Codigo: %d\n", carta2.codigo);
+    printf("Cidade: %s\n", carta2.cidade);
+    printf("Populacao: %d\n", carta2.populacao);
+    printf("Area: %.2f km quadrados\n", carta2.area);
+    printf("PIB: %.2f bilhoes\n", carta2.pib);
+    printf("N de pontos turisticos: %d\n", carta2.NPT);
+
     
-    // Cadastro das Cartas:
+        
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
